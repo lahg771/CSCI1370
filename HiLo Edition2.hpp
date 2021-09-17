b3bdf9033c37e1f09b8a70961128230a8dba65f8
Leslie A. Hurtado <lesliehurtado1103@gmail.com>
2021-09-17 18:12:51 -0500

    Add files via upload

---
+++ HiLo Edition2.hpp
@@ -0,0 +1,13 @@
+//
+//  HiLo Edition2.hpp
+//  Lab 4
+//
+//  Created by Leslie Hurtado on 9/17/21.
+//
+
+#ifndef HiLo_Edition2_hpp
+#define HiLo_Edition2_hpp
+
+#include <stdio.h>
+
+#endif /* HiLo_Edition2_hpp */
